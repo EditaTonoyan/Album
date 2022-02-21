05cd3b606c6eb5b4aac5c967df310e21e909ec46
EditaTonoyan <edita.tonoyan96@gmail.com>
2022-02-21 17:59:06 +0400

    test

--- src/store/redusers/globalReducer.js
+++ src/store/redusers/globalReducer.js
@@ -1,24 +1,24 @@
 const initialState = {
-    image: [],
-    isOpenSpinner: false,
-}
+  image: [],
+  isOpenSpinner: true,
+};
 
 const globalReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "get_images": {
-            return {
-                ...state,
-                image: action.data
-
-            }
-        }
-        case "set_or_remuve_spinner": {
-            return {
-                ...state,
-                isOpenSpinner: action.isOpenSpinner
-            }
-        }
-        default: return state
+  switch (action.type) {
+    case "get_images": {
+      return {
+        ...state,
+        image: action.data,
+      };
     }
-}
-export default globalReducer
+    case "set_or_remuve_spinner": {
+      return {
+        ...state,
+        isOpenSpinner: action.isOpenSpinner,
+      };
+    }
+    default:
+      return state;
+  }
+};
+export default globalReducer;
